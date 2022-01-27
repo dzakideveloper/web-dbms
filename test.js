4430ac6837364b13da24ab771abb8e48778d20bd
dzakideveloper <dzakideveloper7a@gmail.com>
2022-01-27 14:13:49 +0700

    test

---
+++ test.js
@@ -0,0 +1,16 @@
+let a = {
+    kendaraan : {
+        mobil : 'peugeot',
+        motor : 'v-xion',
+        sepeda : 'united'
+    },
+    jalan : 'berkah',
+    kota : 'bekasi',
+    platNomor : 'jabodetabek',
+    kereta : {
+        namaKa : 'argo bromo',
+        tujuan : 'pasar senen'
+    }
+}
+
+console.table(a)
